6bd2722101261675235a15ad4bf0eab62fe8eae3
Alberto García <UnZurdo@users.noreply.github.com>
2018-01-03 01:05:28 +0100

    Update Cliente.cpp

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -10,23 +10,32 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "Socket.h"
 
 using namespace std;
 
-
-/*
- 	-Limitar la cantidad de dinero del cliente???
- 	- Establecer una cantidad inicial
-
-*/
 const int MESSAGE_SIZE = 4001; //mensajes de no más 4000 caracteres
+
+void handle_sigalrm(int signo){
+	signal(SIGINT, handle_sigalrm);
+}
 
 int main(int argc, char* argv[]) {
 	string SERVER_ADDRESS = argv[1];
 	int SERVER_PORT = atoi(argv[2]);
 
-	const string MENS_FIN("EXIT");
+	const string MENS_FIN("exit");
+	const string LLENO ("Vuelo esta LLENO");
+	const string RECHAZADO("rechazado");
+	const string ACEPTADO("aceptado");
+	const string OCUPADO("Asiento solicidado ya esta OCUPADO");
+
+	// Protegemos frente señal
+	signal(SIGINT, handle_sigalrm);
+
     // Dirección y número donde escucha el proceso servidor
 
 	// Creación del socket con el que se llevará a cabo
@@ -34,7 +43,7 @@ int main(int argc, char* argv[]) {
 	Socket socket(SERVER_ADDRESS, SERVER_PORT);
 
     // Conectamos con el servidor. Probamos varias conexiones
-	const int MAX_ATTEMPS = 10;
+	const int MAX_ATTEMPS = 3;
 	int count = 0;
 	int socket_fd;
 	do {
@@ -53,12 +62,54 @@ int main(int argc, char* argv[]) {
     	return socket_fd;
     }
 
-    cout << "EXIT para finalizar operación"<<endl;
-    string mensaje;
+    // Recibimos la respuesta del servidor 
+    string aux; 
+    int read_bytes = socket.Recv(socket_fd, aux, MESSAGE_SIZE);
+    // Cerramos Socket
+    if(aux==RECHAZADO){
+
+    	int error_code = socket.Close(socket_fd);
+	    if(error_code == -1){
+			cerr << "Error cerrando el socket: " << strerror(errno) << endl;
+	    }
+	    cout << "Conexión RECHAZADA"<<endl;
+	    return error_code;
+    }
+
+    cout << "\"exit\" para finalizar operación"<<endl;
+    cout << "Para seleccionar asiento escriba: fila columna"<<endl;
+    string mensaje = "";
+
+    // Buffer para almacenar la respuesta
+	string buffer;
+	bool mensajeContinua = false;
+    int i = 0;
 	do{
-		// Leer mensaje de la entrada estandar
-		cout << "Que asiento desea reservar: ";
-		getline(cin, mensaje);
+
+		if(buffer==OCUPADO){
+			mensaje = "OK";
+			mensajeContinua = true;
+		}
+		// Envio señal de finalizacion
+		else if(i==4) {
+			mensaje = MENS_FIN;
+			cout << "Enviadas demasiadas solicitudes erroneas"<<endl;
+		}
+		else if(buffer == LLENO){
+			mensaje = MENS_FIN;
+			cout << "Vuelo LLENO"<<endl;
+		}
+		else {
+			// Leer mensaje de la entrada estandar
+			cout << "Que asiento desea reservar: ";
+			getline(cin, mensaje);
+			// Caso usuario no introduce nada, repetimo
+			while(mensaje=="") {
+				cout << "Que asiento desea reservar: ";
+				getline(cin, mensaje);
+			}
+		}
+
 		// Enviamos el mensaje
 	    int send_bytes = socket.Send(socket_fd, mensaje);
 
@@ -70,14 +121,19 @@ int main(int argc, char* argv[]) {
 		}
 
 		if(mensaje != MENS_FIN){
-		    // Buffer para almacenar la respuesta
-		    string buffer;
-
-		    // Recibimos la respuesta del servidor  
-		    int read_bytes = socket.Recv(socket_fd, buffer, MESSAGE_SIZE);
-
-		    // Mostramos la respuesta
-		    cout << "RESPUESTA: " << buffer << endl;
+		    if(mensajeContinua){
+		    	int read_bytes = socket.Recv(socket_fd, buffer, MESSAGE_SIZE);
+		    	// Mostramos asientos libres
+		    	cout << buffer << endl;
+		    	mensajeContinua=false;
+		    	++i;
+		    }
+		    else{
+			    // Recibimos la respuesta del servidor  
+			    int read_bytes = socket.Recv(socket_fd, buffer, MESSAGE_SIZE);
+			    // Mostramos la respuesta
+			    cout << "RESPUESTA: " << buffer << endl;
+		    }
 		}
 	} while(mensaje != MENS_FIN);
 
