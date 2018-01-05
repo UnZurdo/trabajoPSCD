f8f0f7a0f20b274dcebfb80a142adb3c7d0aa1d1
jorgegene <32309214+jorgegene@users.noreply.github.com>
2018-01-05 20:06:57 +0100

    Update Valla.cpp

--- src/servidor/Valla.cpp
+++ src/servidor/Valla.cpp
@@ -1,1 +1,30 @@
+//*****************************************************************
+// File:   Valla.h
+// Date:   december 2017
+// Coms:   TP6 PSCD
+//         Compilar mediante
+//           g++ -std=c++11 -pthread Valla.cpp -c
+//*****************************************************************
 
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+void crear::Valla(Valla& s){
+}
+
+
+void actualizarUrl::Valla(Valla& s, string url){
+}
+
+
+void actualizarPath::Valla(Valla& s, string path){
+}
+
+string obtenerUrl(const Valla& s){
+  
+}
+string obtenerPath(const Valla& s){
+  
+}
