429f98a44bd88b3381937d0eb2e5efca6d203f92
jorgegene <32309214+jorgegene@users.noreply.github.com>
2018-01-05 20:02:01 +0100

    Update Administrador.h

--- src/servidor/Administrador.h
+++ src/servidor/Administrador.h
@@ -1,5 +1,5 @@
 //*****************************************************************
-// File:   Monitor.h
+// File:   Administrador.h
 // Date:   december 2017
 // Coms:   TP6 PSCD
 //         Compilar mediante
