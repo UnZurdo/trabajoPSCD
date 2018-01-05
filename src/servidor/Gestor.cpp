08636c52444ed1973c0f9d01c7c3de8d3f1ee9d6
Alberto García <UnZurdo@users.noreply.github.com>
2018-01-05 20:24:45 +0100

    Update Gestor.cpp

--- src/servidor/Gestor.cpp
+++ src/servidor/Gestor.cpp
@@ -6,15 +6,6 @@
 //           g++ -std=c++11 -pthread Subasta.cpp -c
 //*****************************************************************
 
-#include <iostream>
-#include <ctime>
-#include <iomanip>
-#include <queue>
-#include "ImageDownloader.hpp"
-#include "CImg.h"
-#include "Valla.h"
-
-
 Gestor::Gestor();{
     fin = false;
 }
