660474ae12983908aef2f45a2ed8190d5367a342
jorgegene <32309214+jorgegene@users.noreply.github.com>
2018-01-05 20:01:40 +0100

    Update Administrador.cpp

--- src/servidor/Administrador.cpp
+++ src/servidor/Administrador.cpp
@@ -1,3 +1,10 @@
+//*****************************************************************
+// File:   Administrador.cpp
+// Date:   december 2017
+// Coms:   TP6 PSCD
+//         Compilar mediante
+//           g++ -std=c++11 -pthread Subasta.cpp -c
+//*****************************************************************
 #include "Administrador.h"
 
 Administrador::Administrador(Gestor* gestor, Subasta* subasta, Monitor* monitor){
