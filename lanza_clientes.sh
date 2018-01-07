c68eff82b804687485f1037038d52bcefa9696d6
xstarainx <32374903+xstarainx@users.noreply.github.com>
2018-01-07 14:04:23 +0100

    Update lanza_clientes.sh

--- lanza_clientes.sh
+++ lanza_clientes.sh
@@ -6,5 +6,5 @@ echo "Puerto seleccionado: $2"
 echo "Lanzando clientes..."
 
 xterm -e ./bin/Cliente $1 $2 Alberto https://www.w3schools.com/w3css/img_fjords.jpg &
-xterm -e ./bin/Cliente $1 $2 Miguel https://cdn.pixabay.com/photo/2013/04/06/11/50/image-editing-101040_960_720.jpg &
+xterm -e ./bin/Cliente $1 $2 Miguel http://1.bp.blogspot.com/-iS3Rk8DF7qA/UIbHvnp5CtI/AAAAAAAAAEs/M4GJtJTUDWM/s1600/zurdo+4.jpg &
 xterm -e ./bin/Cliente $1 $2 Jorge  https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg &
