78e322acfe3916b3d9292464add860fe89bc36a8
Alberto García <UnZurdo@users.noreply.github.com>
2018-01-14 21:45:01 +0100

    Update lanza_robots.sh

--- lanza_robots.sh
+++ lanza_robots.sh
@@ -6,6 +6,6 @@ echo "Puerto seleccionado: $2"
 echo "Lanzando clientes..."
 
 # https://www.w3schools.com/w3css/img_fjords.jpg
-xterm -e ./bin/ClienteAuto $1 $2 http://www.gettyimages.es/gi-resources/images/Embed/new/embed2.jpg &
+xterm -e ./bin/ClienteAuto $1 $2 https://www.w3schools.com/w3css/img_fjords.jpg &
 xterm -e ./bin/ClienteAuto $1 $2 https://i.pinimg.com/originals/99/a8/b3/99a8b3766f813918c235dda4039f411d.jpg &
 xterm -e ./bin/ClienteAuto $1 $2 http://www.thinkstockphotos.com/ts-resources/images/home/TS_AnonHP_462882495_01.jpg &
